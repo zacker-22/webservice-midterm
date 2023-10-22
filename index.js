d32f818e9b2b538cf93dc53fc9858e1ffa84132c
zacker-22 <shayash72@gmail.com>
2023-10-22 15:22:42 -0700

    Changed port, Added Azure config.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import aviationstack from './routes/flights.js';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
@@ -11,7 +11,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, () => {
+
+app.listen(port, () => {
     console.log(`App started listening at http://localhost:${port}`);
 });
 
